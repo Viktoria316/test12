835240d99d0c34ace4aa479d2eec5947d2d05267
Viktori <viktoria4283@gmail.com>
2024-08-13 22:39:50 +0400

    Initial commit

--- module6hard.py
+++ module6hard.py
@@ -146,13 +146,78 @@ class Figure:
         return self.__color
 
     def __is_valid_color(self, r, g, b):
+        if (r and g and b) == range(0, 255):
+            self.set_color(r, g, b)
+        else:
+            print('Некорректные цвета')
+
+
+    def set_color(self, r, g, b):
+        self.__color.append(r, g, b)
+
+    def __is_valid_sides(self, __sides):
         pass
 
-class Circle:
-    pass
+    def get_sides(self):
+        return self.__sides
 
-class Triangle:
-    pass
+    def __len__(self):
+        pass
 
-class Cube:
-    pass
+    def set_sides(self, *new_sides):
+        pass
+
+
+
+
+
+class Circle(Figure):
+    sides_count = 1
+    __radius = 0
+
+    def __init__(self, (r, g, b), __radius):
+        super().set_color((r, g, b))
+
+
+    def get_square(self):
+        pass
+
+
+
+class Triangle(Figure):
+    sides_count = 3
+    __height = 0
+
+    def get_square(self):
+        pass
+
+
+
+class Cube(Figure):
+    sides_count = 12
+    __sides = sides_count
+
+    def get_volume(self):
+        pass
+
+
+circle1 = Circle((200, 200, 100), 10) # (Цвет, стороны)
+cube1 = Cube((222, 35, 130), 6)
+
+# Проверка на изменение цветов:
+circle1.set_color(55, 66, 77) # Изменится
+print(circle1.get_color())
+cube1.set_color(300, 70, 15) # Не изменится
+print(cube1.get_color())
+
+# Проверка на изменение сторон:
+cube1.set_sides(5, 3, 12, 4, 5) # Не изменится
+print(cube1.get_sides())
+circle1.set_sides(15) # Изменится
+print(circle1.get_sides())
+
+# Проверка периметра (круга), это и есть длина:
+print(len(circle1))
+
+# Проверка объёма (куба):
+print(cube1.get_volume())
