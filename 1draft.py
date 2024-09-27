a967cbd8e25578ed7fafa3c5b34e3d97dfd598ce
Viktori <viktoria4283@gmail.com>
2024-09-27 23:12:54 +0400

    Initial commit

--- 1draft.py
+++ 1draft.py
@@ -1,49 +1,57 @@
-g = 'Hello, world!'
+class StepValueError(ValueError):
+  pass
 
-x = 78
-y = 54
+class Iterator:
 
-'''
-# print(x + y)
-# print(x - y)
-print(x * y)
-print(x / y)
-print(x // y)
-print('я ЗДЕСЬ')
-'''
+  def __init__(self, start, stop, step=1):
+    if step == 0:
+      raise StepValueError('Шаг не может быть равен 0')
+    self.start = start
+    self.stop = stop
+    self.step = step
+    self.pointer = start
 
-a = 76
-print(type(a))
-b = 'BoSin'
-print(type(b))
-c = 6.8
-print(type(c))
-d = False
-print(type(d))
+  def __iter__(self):
+    """Сбрасывает значение pointer на start и возвращает сам объект итератора."""
+    self.pointer = self.start
+    return self
 
+  def __next__(self):
 
+    if self.step > 0 and self.pointer >= self.stop:
+      raise StopIteration
+    if self.step < 0 and self.pointer <= self.stop:
+      raise StopIteration
+    current_value = self.pointer
+    self.pointer += self.step
+    return current_value
 
+# Пример выполняемого кода:
+try:
+  iter1 = Iterator(100, 200, 0)
+  for i in iter1:
+    print(i, end=' ')
+except StepValueError:
+  print('Шаг указан неверно')
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+iter2 = Iterator(-5, 1)
+iter3 = Iterator(6, 15, 2)
+iter4 = Iterator(5, 1, -1)
+iter5 = Iterator(10, 1)
 
 
-
-
+for i in iter2:
+  print(i, end=' ')
+print()
+for i in iter3:
+  print(i, end=' ')
+print()
+for i in iter4:
+  print(i, end=' ')
+print()
+for i in iter5:
+  print(i, end=' ')
+print()
 
 
 
