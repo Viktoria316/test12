2addd813de9dcd0fd6b279037181cd83b1541010
Viktori <viktoria4283@gmail.com>
2024-09-27 23:12:54 +0400

    Initial commit

--- module_9_3.py
+++ module_9_3.py
@@ -29,4 +29,9 @@ print(list(second_result))
 first = ['Strings', 'Student', 'Computers']
 second = ['Строка', 'Урбан', 'Компьютер']
 
-first_result = zip(first, second)
+first_result = (len(first_word) - len(second_word) for first_word, second_word in zip(first, second) if len(first_word) != len(second_word))
+
+second_result = (len(first[i]) == len(second[i]) for i in range(len(first)))
+
+print(list(first_result))
+print(list(second_result))
